6b3ed954dadc6234d2b22ac283c639eb00789b38
atit <atittulachan6072@gmail.com>
2026-07-05 22:56:11 +0545

    add: brand model

--- src/models/brand.model.ts
+++ src/models/brand.model.ts
@@ -1,5 +1,26 @@
-// name ,description, logo
+import mongoose  from "mongoose";;
 
-// brand schema
+// brand schema (name ,description, logo)
+const brandSchema = new mongoose.Schema({
+    name:{
+        type : String,
+        required : [true, "brand_name is required"],
+        minLength: [3, "name must be at least 3 character long"],
+        trim: true,
+    },
+    description : {
+        type : String,
+        required : [true,"description is required"],
+        minLength: [3, "name must be at least 3 character long"],
+        trim: true,
+    },
+    logo : {
+        type : String
+
+    }
+})
 
 // brand model
+const Brand = mongoose.model("brand",brandSchema);
+
+export default Brand;
