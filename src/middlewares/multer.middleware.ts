27904d512991e24f26503919cda95983a0bf0020
atit <atittulachan6072@gmail.com>
2026-07-09 00:12:12 +0545

    add: implement multer middleware for file uploads and update auth routes

---
+++ src/middlewares/multer.middleware.ts
@@ -0,0 +1,58 @@
+import multer, { FileFilterCallback } from "multer";
+import fs from "fs";
+import { Request } from "express";
+import AppError from "../utils/appError.utils";
+
+export const uploader = () => {
+    const folder = "uploads/";
+    const fileSize = 5 * 1024 * 1024;
+
+    if (!fs.existsSync(folder)){
+        fs.mkdirSync(folder, {recursive : true})
+    }
+
+    // multer disk storage
+  const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, folder);
+    },
+    filename: (req, file, cb) => {
+      const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+      cb(null, file.fieldname + "-" + uniqueSuffix + "-" + file.originalname);
+    },
+  });
+
+  // file filter
+  const fileFilter = (
+    req: Request,
+    file: Express.Multer.File,
+    callback: FileFilterCallback,
+  ) => {
+    const allowed_mime_type = [
+        'image/jpg',
+        'image/jpeg',
+        'image/png',
+        'image/svg+xml',
+        'doc/pdf',
+        'image/webp'
+
+    ]
+    if (!allowed_mime_type.includes(file.mimetype)){
+        callback (new AppError(`${file.mimetype} is not allowed`,422))
+    }else{
+        callback(null ,true);
+    }
+  };
+
+  // multer upload instance
+  const upload = multer({
+    storage: storage,
+    fileFilter : fileFilter,
+    limits: {
+      fileSize: fileSize,
+    },
+  });
+
+  return upload;
+};
+
--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -2,11 +2,14 @@ import express from "express";
 
 import { login, register } from "../controllers/auth.controller";
 
-import multer from "multer";
-
+import { uploader } from "../middlewares/multer.middleware";
 const router = express.Router()
 
-const upload = multer({dest:'uploads/'})
+// const upload = multer({dest:'uploads/'})
+const upload = uploader()
+
+router.post("/register", upload.single("profile_image"), register);
+
 
 //* register
 router.post("/register",register);
