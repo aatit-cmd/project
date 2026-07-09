a1d6ba12c6b43363708683e5cf0dcba6fb607e69
atit <atittulachan6072@gmail.com>
2026-07-09 16:03:08 +0545

    add: configure environment variables and integrate Cloudinary for file uploads

---
+++ src/config/cloudinary.config.ts
@@ -0,0 +1,9 @@
+import {v2 as cloudinary} from "cloudinary"
+
+cloudinary.config({ 
+  cloud_name: 'wj1idmcy', 
+  api_key: '243274645877424', 
+  api_secret: 'AWzvEfAPVh7wVt3vVapGureyiXQ'
+});
+
+export default cloudinary;
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import app from "./app";
 import { connectDatabase } from "./config/db.config";
-const PORT =8080;
-const DBI_URI = "mongodb://localhost:27017/project"
+
+const PORT = process.env.PORT;
+const DBI_URI = process.env.DBI_URI ?? "";
+
 //* connect database
 connectDatabase(DBI_URI);
 
 //* listen
-app.listen(PORT,()=>{
-    console.log(`Server is running at http://localhost:${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
