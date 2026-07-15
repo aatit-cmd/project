8dc7b081a6e232d5122a7154cd706329ea99b773
atit <atittulachan6072@gmail.com>
2026-07-15 16:28:18 +0545

    fix: correct formatting and add missing newline in image model

--- src/models/image.model.ts
+++ src/models/image.model.ts
@@ -1,18 +1,21 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
 const ImageSchema = new mongoose.Schema(
-    {
-            path: {
-              type: String,
-              required: [true, "path is required"],
-            },
-            public_id: {
-              type: String,
-              required: [true, "public_id is required"],
-            },
-          },{
-          _id: false,
-        },
-    );
+  {
+    path: {
+      type: String,
+      required: [true, "path is required"],
+    },
+    public_id: {
+      type: String,
+      required: [true, "public_id is required"],
+    },
+  },
+  {
+    _id: false,
+  },
+);
 
-export default ImageSchema
+export default ImageSchema;
+
+
