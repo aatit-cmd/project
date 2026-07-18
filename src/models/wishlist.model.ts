b6e73f49e9a99a891f5f32c7816b2bdadee07276
atit <atittulachan6072@gmail.com>
2026-07-18 13:58:26 +0545

    add: wishlist model

--- src/models/wishlist.model.ts
+++ src/models/wishlist.model.ts
@@ -1,3 +1,36 @@
 // product: id, user: id
 
 // {user:user_id, items[{product:product_id, quantity:number}]}
+
+import mongoose from "mongoose";
+import { minLength } from "zod";
+
+const wishlistSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+      required: [true, "user_id is required"],
+    },
+    items: [
+      {
+        productId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Product",
+          required: [true, "product_id is required"],
+        },
+        quantity: {
+          type: Number,
+          required: [true, "quantity is required"],
+          minLength: [1, "quantity cannot be less than 1"],
+          default: 1,
+        },
+      },
+    ],
+  },
+  { timestamps: true },
+);
+
+const wishlist = mongoose.model("wishlist", wishlistSchema);
+
+export default wishlist;
